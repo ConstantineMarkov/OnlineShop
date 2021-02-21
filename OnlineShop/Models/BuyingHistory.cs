6e3b31e1c8c5e55fbf725a2df7a63e39cf468176
Koceto <kosyo.marko@gmail.com>
2021-02-21 12:07:13 +0200

    ProjectConnectedToDatabase

---
+++ OnlineShop/Models/BuyingHistory.cs
@@ -0,0 +1,17 @@
+﻿// <copyright file="BuyingHistory.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace OnlineShop.Models
+{
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    [Table("BuyingHistory")]
+    public class BuyingHistory
+    {
+
+        public int UserId { get; set; }
+        public int ProductId { get; set; }
+    }
+}
---
+++ OnlineShop/Models/ProductModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace OnlineShop.Models
+{
+    [Table("Products")]
+    public class ProductModel
+    {
+        [Key]
+        [Column("Id")]
+        [Required]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ProductId { get; set; }
+
+        [Required]
+        [Column("Category")]
+        public string Category { get; set; }
+
+        [Required]
+        [Column("Price")]
+        public int Price { get; set; }
+
+        [Required]
+        [Column("Count")]
+        public int Count { get; set; }
+
+        [Column("Description")]
+        public string Description { get; set; }
+
+        [ForeignKey("ProductFK")]
+        public ICollection<ReviewModel> Reviews { get; set; }
+    }
+}
