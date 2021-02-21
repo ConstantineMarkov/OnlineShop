6c395be55b74dcd0c3276845ffb795f173ed767d
Koceto <kosyo.marko@gmail.com>
2021-02-21 14:51:15 +0200

    No Home

---
+++ OnlineShop/Migrations/20210221111034_FirstSeedOnProductTable.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OnlineShop.Migrations
+{
+    public partial class FirstSeedOnProductTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Category", "Count", "Description", "Price" },
+                values: new object[] { 1, "TShirt", 10, "Rand Desc", 20 });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1);
+        }
+    }
+}
